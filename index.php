6ebe60c7a543a603526cb99c240ed3047652c828
Tomokatsu@ubuntu14 <tomo14916253@gmail.com>
2016-06-29 23:03:11 +0900

    test

---
+++ index.php
@@ -0,0 +1,12 @@
+<?php 
+
+$get = $_GET;
+
+$result = array("response_type" => "in_channel",
+    		"text"          => "成功.",
+    		"attachments"   => [
+        		array("text" => "うまくいったぜ")
+		]
+);
+
+echo JSON.parse($result);
