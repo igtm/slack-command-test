fa565c66a0440985a916c648a02c9d123e61abd7
Tomokatsu@ubuntu14 <tomo14916253@gmail.com>
2016-06-29 23:54:03 +0900

    test

--- index.php
+++ index.php
@@ -1,7 +1,21 @@
 <?php 
 
-$get = $_GET;
-try{
+const TOKEN = "iKwPblls7qGNycujAdgblhm8";
+
+$token        = $_GET["token"];
+$team_id      = $_GET["team_id"];
+$team_domain  = $_GET["team_domain"];
+$channel_id   = $_GET["channel_id"];
+$channel_name = $_GET["channel_name"];
+$user_id      = $_GET["user_id"];
+$user_name    = $_GET["user_name"];
+$command      = $_GET["command"];
+$text         = $_GET["text"];
+$response_url = $_GET["response_url"];
+
+if ($TOKEN !== $token) throw new Exception();
+
+
 $result = array("response_type" => "in_channel",
 				"text"          => "成功.",
 				"attachments"   => [
@@ -9,7 +23,5 @@ $result = array("response_type" => "in_channel",
 				]
 );
 
-echo json_encode($get);
-}catch(Exception $e){
-echo $e;
-}
+header('Content-Type: application/x-www-form-urlencoded');
+echo json_encode($result);
