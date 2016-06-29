a61424743a397cfaad2574c1e504491d65d23351
Tomokatsu@ubuntu14 <tomo14916253@gmail.com>
2016-06-29 23:24:18 +0900

    test

--- index.php
+++ index.php
@@ -3,10 +3,10 @@
 $get = $_GET;
 try{
 $result = array("response_type" => "in_channel",
-    		"text"          => "成功.",
-    		"attachments"   => [
-        		array("text" => "うまくいったぜ")
-		]
+				"text"          => "成功.",
+				"attachments"   => [
+					array("text" => "うまくいったぜ")
+				]
 );
 
 echo json_encode($result);
